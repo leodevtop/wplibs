4a279064a0008516d27f81fd0d30532214cdf504
Gioan Vu <vntuanbk@gmail.com>
2021-12-08 02:57:50 -0800

    commit 2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '526045fae8a1a3fe818ee540d49a360fdebe63cd',
+    'name' => 'vust/wplibs',
+  ),
+  'versions' => 
+  array (
+    'google/recaptcha' => 
+    array (
+      'pretty_version' => '1.2',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3875d025d0465f36f32452f614e7ffc1462c889f',
+    ),
+    'vust/wplibs' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '526045fae8a1a3fe818ee540d49a360fdebe63cd',
+    ),
+  ),
+);
